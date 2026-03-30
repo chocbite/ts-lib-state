00649a9c525922294f7052612162c57c21194739
Chocolateandmilkwin <nicknielsen1995@hotmail.com>
2026-03-30 22:49:12 +0200

    Trial not failing

--- src/trials/trials.ts
+++ src/trials/trials.ts
@@ -1,10 +1,12 @@
 import { ok } from "@chocbite/ts-lib-result";
 import state from "..";
 
-const state_inst = state.rosw(state.o.help(ok({ a: 5 })));
+const state_inst = state.rosw(
+  state.o.help(ok({ a: 5 } as { [key: string]: number })),
+);
 
 state_inst.sub((v) => {
   console.warn(v.value);
 }, true);
 state_inst.write(state.o.write.add({ c: 10 }));
-state_inst.object.add("b", 10);
+state_inst.object.add(4, 10);
